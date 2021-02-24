bf4d087284267d933647aff04f226ae3a082bcfd
Isuru <57609424+1996-isuru@users.noreply.github.com>
2021-02-24 22:06:57 +0530

    removed

--- Dashboard/common/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-SESSION_START();
-$_SESSION['username'] = "";
-$_SESSION['userid']  = "";
-$_SESSION['login_details_id']= "";
-header("Location:index.php");
-?>
--- common/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-SESSION_START();
-$_SESSION['username'] = "";
-$_SESSION['user_id']  = "";
-$_SESSION['login_details_id']= "";
-header("Location:../login/index.php");
-?>
